f927c8f9d580820501e9e40ebacaef6e3db610c8
Jacob <frankljbe@gmail.com>
2015-10-25 16:54:25 -0400

    [utils]Commiting partial custom priority queue
    commiting partial implementation so I can work on another comp.

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -1,100 +1,20 @@
 package tests;
 
 import (
-    "log"
-    "time"
-    "runtime"
     "testing"
     "github.com/wenkesj/rphash/utils"
+    "github.com/wenkesj/rphash/itemset"
 );
 
-func primeSieve(n uint) []bool {
-    sieve := make([]bool, n+1);
-    half := n / 2;
-
-    for idx := range sieve {
-        sieve[idx] = true;
-    }
-
-    for i := uint(2); i <= half; i++ {
-        if sieve[i] {
-            for j := i + i; j <= n; j += i {
-                sieve[j] = false;
-            }
-        }
-    }
-
-    return sieve;
-};
-
-func primes(n uint) []uint {
-    r := make([]uint, 0);
-
-    sieve := primeSieve(n);
-
-    for i := uint(2); i <= n; i++ {
-        if sieve[i] {
-            r = append(r, i);
-        }
-    }
-
-    return r;
-};
-
-func primeFactors(primes []uint, n uint) []uint {
-    factors := make([]uint, 0);
-
-    for {
-        if n <= 1 {
-            break;
-        }
-
-        d := n / primes[0];
-        r := n % primes[0];
-
-        if r == 0 {
-            factors = append(factors, primes[0]);
-            n = d;
-        } else {
-            if len(primes) == 1 {
-                panic("not enough primes available to factorize number");
-            }
-            primes = primes[1:];
-        }
-    }
-
-    return factors;
-};
-
-func forLinear(begin, end, step uint, f func(uint)) error {
-    for i := begin; i < end; i += step {
-        f(i);
-    }
-
-    return nil;
-};
-
-func TestParallelFor(t *testing.T) {
-    nC := runtime.NumCPU();
-    runtime.GOMAXPROCS(nC);
-
-    log.Println("The number of CPUs allocated: ",nC);
-
-    var max uint = 1000;
-
-    p := primes(uint(max));
-    factors := make([][]uint, max);
-
-    f := func(n uint) {
-        factors[n] = primeFactors(p, uint(n));
-    };
-
-    timeStart := time.Now();
-    parallel.For(0, max, 2, f);
-    log.Println("Interleaved (Parallel): ",time.Now().Sub(timeStart));
-
-    timeStart = time.Now();
-    forLinear(0, max, 2, f);
-    log.Println("Linear (Non-Parallel): ",time.Now().Sub(timeStart));
-    log.Println("parallel\x1b[32;1m √\x1b[0m");
-};
+func TestPriorityQueueEnque(t *testing.T) {
+  fakeCentriod := itemset.NewCentroidSimple(1, 1);
+  var countlist map[int32]int32;
+  priorityQueue := utils.NewPQueue(countlist);
+  if priorityQueue.Size() != 0 {
+    t.Error("priorityQueue size is not 0 before Enqueing.")
+  }
+  priorityQueue.Enque(fakeCentriod);
+  if priorityQueue.Size() != 1 {
+    t.Error("priorityQueue size is not 1 after Enqueing once.")
+  }
+}
--- utils/pqueue.go
+++ utils/pqueue.go
@@ -2,40 +2,94 @@ package utils;
 
 import (
     "github.com/wenkesj/rphash/types"
+    "math"
 );
 
 type PQueue struct {
-    plist map[int32]int32;
+    plist map[int32]int32; //What is the point of this?
+    heap []types.Centroid;
+    heapSize int;
 };
 
 func NewPQueue(plist map[int32]int32) *PQueue {
+    heapSize = 0;
+    heap := [16]types.Centroid;
     return &PQueue{plist:plist};
 };
 
-func (this *PQueue) Compare(x, y types.Centroid) int {
-    cx := this.plist[x.GetID()];
-    cy := this.plist[y.GetID()];
-    if cx > cy {
+func (this *PQueue) Deque() (types.Centroid, errors) {
+    if(heapSize < 1) {
+      return nil, errors.New("Queue contains no centriods");
+    }
+    result = heap[0];
+    heap[0] = heap[heapSize - 1];
+    this.percolateDown(0);
+    heapSize--;
+    return result, nil;
+};
+
+func (this *PQueue) Enque(newCentriod types.Centroid) {
+  heapSize++;
+  heap[heapSize] = newCentriod;
+  this.percolateUp(heapSize);
+};
+
+//The children of index X are (2^X) and (2^X) + 1
+func (this *PQueue) percolateUp(lowerIndex int){
+  if(lowerIndex == 0) {
+    return;
+  }
+  if(lowerIndex % 2 == 0) {
+    lowerIndex = lowerIndex - 1;
+  }
+  var upperIndex = lowerIndex / 2;
+  if(this.compare(lowerIndex, upperIndex) < 0) {
+    swap(lowerIndex, upperIndex);
+    if(upperIndex != 0)
+      this.percolateUp(upperIndex);
+    }
+  }
+  //Else we have fixed the priorityQueue;
+}
+
+func (this *PQueue) percolateDown(upperIndex int){
+  var lowerIndex = 2 * upperIndex;
+  if(lowerIndex <= this.heapSize) {
+    return; // If this node has no children we are done.
+  }
+  if(this.compare(lowerIndex, upperIndex) < 0) {
+    swap(lowerIndex, upperIndex);
+    this.percolateDown(lowerIndex);
+  }
+  //Else we have fixed the priorityQueue;
+}
+
+func (this *PQueue) swap(index1 int, index2 int){
+    var temp = heap[index1];
+    heap[index1] = heap[index2];
+    heap[index2] = temp;
+}
+
+func (this *PQueue) compare(centriod1 types.Centroid, centriod2 types.Centroid) int {
+    id1 := centriod1.GetID(); //this.plist[centriod1.GetID()];
+    id2 := centriod2.GetID()//this.plist[centriod2.GetID()];
+    if id1 > id2 {
         return 1;
-    } else if cx < cy {
+    } else if id1 < id2 {
         return -1;
     }
     return 0;
 };
 
-func (this *PQueue) Remove(rem types.Centroid) (a types.Centroid) {
+func (this *PQueue) compare(index1 int, index2 int) int {
+  return compare(heap[index1], heap[index2])
+};
+
+func (this *PQueue) Size() int {
     return a;
 };
 
-func (this *PQueue) Add(rem types.Centroid) {
-
-};
-
-func (this *PQueue) Size() (a int) {
-    return a;
-};
-
-func (this *PQueue) IsEmpty() (a bool) {
+func (this *PQueue) IsEmpty() bool {
     return a;
 };
 
