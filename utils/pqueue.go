af2f0627fad0c8c917dc241947cdd729fe8cbb8d
frankljbe <frankljbe@gmail.com>
2015-10-25 18:29:30 -0400

    [utils] Fixing syntax errors in pqueue

--- utils/pqueue.go
+++ utils/pqueue.go
@@ -1,83 +1,88 @@
-package utils;
+package utils
 
 import (
-    "github.com/wenkesj/rphash/types"
-    "math"
-);
+	"errors"
+	"github.com/wenkesj/rphash/types"
+)
 
-<<<<<<< HEAD
 type PQueue struct {
-    plist map[int32]int32; //What is the point of this?
-    heap []types.Centroid;
-    heapSize int;
-};
+	plist    map[int32]int32 //What is the point of this?
+	heap     []types.Centroid
+	heapSize int
+}
 
 func NewPQueue(plist map[int32]int32) *PQueue {
-    heapSize = 0;
-    heap := [16]types.Centroid;
-    return &PQueue{plist:plist};
-};
+	heap := make([]types.Centroid, 0, 16)
+	return &PQueue{heapSize: 0,
+		heap:  heap,
+		plist: plist}
+}
 
-func (this *PQueue) Deque() (types.Centroid, errors) {
-    if(heapSize < 1) {
-      return nil, errors.New("Queue contains no centriods");
-    }
-    result = heap[0];
-    heap[0] = heap[heapSize - 1];
-    this.percolateDown(0);
-    heapSize--;
-    return result, nil;
-};
+func (this *PQueue) Deque() (types.Centroid, error) {
+	if this.heapSize < 1 {
+    err := errors.New("Queue contains no centriods")
+		return nil, err;
+	}
+	var result = this.heap[0];
+	this.heap[0] = this.heap[this.heapSize];
+	this.percolateDown(0);
+	this.heapSize--;
+	return result, nil;
+}
 
 func (this *PQueue) Enque(newCentriod types.Centroid) {
-  heapSize++;
-  heap[heapSize] = newCentriod;
-  this.percolateUp(heapSize);
-};
+	this.heapSize++;
+	this.heap[this.heapSize] = newCentriod;
+	this.percolateUp(this.heapSize);
+}
 
 //The children of index X are (2^X) and (2^X) + 1
-func (this *PQueue) percolateUp(lowerIndex int){
-  if(lowerIndex == 0) {
-    return;
-  }
-  if(lowerIndex % 2 == 0) {
-    lowerIndex = lowerIndex - 1;
-  }
-  var upperIndex = lowerIndex / 2;
-  if(this.compare(lowerIndex, upperIndex) < 0) {
-    swap(lowerIndex, upperIndex);
-    if(upperIndex != 0)
-      this.percolateUp(upperIndex);
-    }
-  }
-  //Else we have fixed the priorityQueue;
+func (this *PQueue) percolateUp(lowerIndex int) {
+	if lowerIndex == 0 {
+		return;
+	}
+	if lowerIndex%2 == 0 {
+		lowerIndex = lowerIndex - 1;
+	}
+	var upperIndex = lowerIndex / 2;
+	if this.compareAtPositions(lowerIndex, upperIndex) < 0 {
+		this.swap(lowerIndex, upperIndex);
+		if upperIndex != 0 {
+			this.percolateUp(upperIndex);
+		}
+	}
+	//Else we have fixed the priorityQueue;
 }
 
-func (this *PQueue) percolateDown(upperIndex int){
-  var lowerIndex = 2 * upperIndex;
-  if(lowerIndex <= this.heapSize) {
-    return; // If this node has no children we are done.
-  }
-  if(this.compare(lowerIndex, upperIndex) < 0) {
-    swap(lowerIndex, upperIndex);
-    this.percolateDown(lowerIndex);
-  }
-  //Else we have fixed the priorityQueue;
+func (this *PQueue) percolateDown(upperIndex int) {
+	var lowerIndex = 2 * upperIndex;
+	if lowerIndex <= this.heapSize {
+		return // If this node has no children we are done.
+	}
+	if this.compareAtPositions(lowerIndex, upperIndex) < 0 {
+		this.swap(lowerIndex, upperIndex);
+		this.percolateDown(lowerIndex);
+	}
+	//Else we have fixed the priorityQueue;
 }
 
-func (this *PQueue) swap(index1 int, index2 int){
-    var temp = heap[index1];
-    heap[index1] = heap[index2];
-    heap[index2] = temp;
+func (this *PQueue) swap(index1 int, index2 int) {
+  var temp = this.heap[index1];
+	this.heap[index1] = this.heap[index2];
+  this.heap[index2] = temp;
 }
 
 func (this *PQueue) compare(centriod1 types.Centroid, centriod2 types.Centroid) int {
-    id1 := centriod1.GetID(); //this.plist[centriod1.GetID()];
-    id2 := centriod2.GetID()//this.plist[centriod2.GetID()];
-    if id1 > id2 {
-        return 1;
-    } else if id1 < id2 {
-        return -1;
-    }
-    return 0;
-};
+id1 := centriod1.GetID(); //this.plist[centriod1.GetID()];
+	id2 := centriod2.GetID(); //this.plist[centriod2.GetID()];
+	if id1 > id2 {
+		return 1;
+	} else if id1 < id2 {
+		return -1;
+	}
+	return 0;
+}
+
+func (this *PQueue) compareAtPositions(index1 int, index2 int) int {
+  return this.compare(this.heap[index1], this.heap[index2]);
+}
