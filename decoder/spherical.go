7ea5ebb088a3623230a5870dabd92bfa33fe90d8
frankljbe <frankljbe@gmail.com>
2015-11-28 12:37:46 -0500

    [decoder]Variable rename increase readability

--- decoder/spherical.go
+++ decoder/spherical.go
@@ -11,47 +11,47 @@ var HashBits int = 64;
 type Spherical struct {
     vAll [][][]float64;
     hbits int;
-    d int;
-    k int;
-    l int;
+    numDimensions int;
+    numHashFuncs int;
+    numSearchCopies int;
     distance float64;
     variance float64;
 };
 
-func NewSpherical(d, k, l int) *Spherical {
-    nvertex := 2.0 * d;
+func NewSpherical(numDimensions, numHashFuncs, numSearchCopies int) *Spherical {
+    nvertex := 2.0 * numDimensions;
     hbits := int(math.Ceil(math.Log(float64(nvertex)) / math.Log(2)));
     kmax := int(HashBits / hbits);
-    if (k > kmax) {
-        k = kmax;
+    if (numHashFuncs > kmax) {
+        numHashFuncs = kmax;
     }
-    vAll := make([][][]float64, k * l);
-    r := make([]*rand.Rand, d);
-    for i := 0; i < d; i++ {
+    vAll := make([][][]float64, numHashFuncs * numSearchCopies);
+    r := make([]*rand.Rand, numDimensions);
+    for i := 0; i < numDimensions; i++ {
         r[i] = rand.New(rand.NewSource(int64(i)));
     }
     rotationMatrices := vAll;
-    for i := 0; i < k * l; i++ {
-        rotationMatrices[i] = utils.RandomRotation(d, r);
+    for i := 0; i < numHashFuncs * numSearchCopies; i++ {
+        rotationMatrices[i] = utils.RandomRotation(numDimensions, r);
     }
     vAll = rotationMatrices;
     return &Spherical{
         vAll: vAll,
         hbits: hbits,
-        d: d,
-        k: k,
-        l: l,
+        numDimensions: numDimensions,
+        numHashFuncs: numHashFuncs,
+        numSearchCopies: numSearchCopies,
         distance: 0.0,
         variance: 1.0,
     };
 };
 
 func (this *Spherical) GetDimensionality() int {
-    return this.d;
+    return this.numDimensions;
 };
 
 func (this *Spherical) GetErrorRadius() float64 {
-    return float64(this.d);
+    return float64(this.numDimensions);
 };
 
 func (this *Spherical) GetDistance() float64 {
@@ -61,12 +61,12 @@ func (this *Spherical) GetDistance() float64 {
 func (this *Spherical) Hash(p []float64) []int64 {
     ri := 0;
     var h int64;
-    g := make([]int64, this.l);
-    for i := 0; i < this.l; i++ {
+    g := make([]int64, this.numSearchCopies);
+    for i := 0; i < this.numSearchCopies; i++ {
         g[i] = 0;
-        for j := 0; j < this.k; j++ {
+        for j := 0; j < this.numHashFuncs; j++ {
             vs := this.vAll[ri];
-            h = utils.Argmaxi(p, vs, this.d);
+            h = utils.Argmaxi(p, vs, this.numDimensions);
             g[i] |= (h << (uint(this.hbits * j)));
             ri++;
         }
