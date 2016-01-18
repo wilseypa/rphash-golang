23ed504c91d1bc5abed8e55ea1643e23ff44ee05
Jacob <frankljbe@gmail.com>
2016-01-18 12:03:55 -0500

    [utils] Add new util comment

--- utils/bitShift.go
+++ utils/bitShift.go
@@ -1,5 +1,5 @@
 package utils;
-
+// Java like zero extension shift see http://docs.oracle.com/javase/specs/jls/se7/html/jls-15.html#jls-15.19 >>> operator
 func RightShiftZeroExtension(leftOperand int64, rightOperand int64) int64{
   if leftOperand > -1 {
     return leftOperand >> uint64(rightOperand);
