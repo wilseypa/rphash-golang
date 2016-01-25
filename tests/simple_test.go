77b77973fa4578b3934eb2b260679b0962f9ef9f
Jacob <frankljbe@gmail.com>
2016-01-25 11:20:05 -0500

    [test]Compute accuracy of RPHash compared to KMeans
    Now have a test that returns how many rphash clusters match the kmeans
    result. The results look pretty close to random right now.

--- tests/simple_test.go
+++ tests/simple_test.go
@@ -6,9 +6,25 @@ import (
   "github.com/wenkesj/rphash/simple"
   "math/rand"
   "github.com/wenkesj/rphash/clusterer"
+  "github.com/wenkesj/rphash/utils"
+  "github.com/wenkesj/rphash/hash"
+  "github.com/wenkesj/rphash/decoder"
+  "github.com/wenkesj/rphash/projector"
+  "github.com/wenkesj/rphash/lsh"
+  "sort"
 );
-
 func TestSimple(t *testing.T) {
+  //LSH function used for testing results only
+  var seed int64 = 0;
+  var d, k, l int = 10, 6, 4;
+
+  var inDimensions, outDimentions int = 10, 2;
+  hash := hash.NewMurmur(1 << 63 - 1);
+  decoder := decoder.NewSpherical(d, k, l);
+  projector := projector.NewDBFriendly(inDimensions, outDimentions, seed);
+  lsh := lsh.NewLSH(hash, decoder, projector);
+
+
   //Create fake data
   var numClusters = 4;
   var numRows = 100;
@@ -30,13 +46,54 @@ func TestSimple(t *testing.T) {
   if len(RPHashObject.GetCentroids()) != numClusters {
     t.Errorf("Requested %v centriods. But RPHashSimple returned %v.", numClusters, len(RPHashObject.GetCentroids()));
   }
-  t.Log(RPHashObject.GetCentroids());
 
-  //Find clusters using KMeans
+  //Sort centriods by LSH result
+  rpHashMap := make(map[int64][]float64);
+  for _, result := range RPHashObject.GetCentroids() {
+    rpHashMap[lsh.LSHHashSimple(result)] = result;
+  }
+  rpHashResult := make([][]float64, numClusters, numClusters)
+  var keys []int
+  for key := range rpHashMap {
+    keys = append(keys, int(key))
+  }
+  sort.Ints(keys)
+  for i, key := range keys {
+    t.Log(key);
+    rpHashResult[i] = rpHashMap[int64(key)];
+  }
+  //Find clusters using KMeans and sort by LSH result
   clusterer := clusterer.NewKMeansSimple(numClusters, data);
   clusterer.Run();
-  var kMeansResult = clusterer.GetCentroids();
-  t.Log(kMeansResult);
+
+  kMeansMap := make(map[int64][]float64);
+  for _, result := range clusterer.GetCentroids() {
+    kMeansMap[lsh.LSHHashSimple(result)] = result;
+  }
+
+  kMeansResult := make([][]float64, numClusters, numClusters)
+  keys = nil;
+  for key := range kMeansMap {
+    keys = append(keys, int(key))
+  }
+  sort.Ints(keys)
+  for i, key := range keys {
+    kMeansResult[i] = kMeansMap[int64(key)];
+  }
+
+
+  //Assign centriods
+  kMeansAssignments := make([]int, numRows, numRows);
+  rpHashAssignments := make([]int, numRows, numRows);
+  var matchingAssignmentCount = 0;
+  for i, vector := range data {
+    rpHashAssignments[i] = utils.FindNearestDistance(vector, rpHashResult);
+    kMeansAssignments[i] = utils.FindNearestDistance(vector, kMeansResult);
+    if rpHashAssignments[i] == kMeansAssignments[i] {
+      matchingAssignmentCount += 1;
+    }
+  }
+  t.Log("Percent Matching: ", float64(matchingAssignmentCount)/float64(numRows))
 };
 
 /*func BenchmarkSimple(b *testing.B) {
