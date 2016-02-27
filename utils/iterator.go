f909a1776fdc2e6e426271253d6927700165dd49
Jacob <frankljbe@gmail.com>
2016-02-27 17:15:05 -0500

    [utils]Iterator variable rename, add lshval storage

--- utils/iterator.go
+++ utils/iterator.go
@@ -1,33 +1,44 @@
 package utils;
 
 type IterableSlice struct {
-    x int;
-    s [][]float64;
+    position int;
+    data [][]float64;
+    lshVals []int64;
 };
 
-func (s *IterableSlice) Next() (value []float64) {
-    s.x++;
-    return s.s[s.x];
+func (this *IterableSlice) Next() (value []float64) {
+    this.position++;
+    return this.data[this.position];
 };
 
-func (s *IterableSlice) HasNext() (ok bool) {
-    s.x++;
-    if s.x >= len(s.s) {
-        s.x--;
+func (this *IterableSlice) PeakLSH() (lshValue int64) {
+  if(this.lshVals == nil) {
+    panic("Cannot call PeakLSH until after StoreLSHValues");
+  }
+  return this.lshVals[this.position];
+}
+func (this *IterableSlice) StoreLSHValues(lshVals []int64)  {
+  this.lshVals = lshVals;
+}
+
+func (this *IterableSlice) HasNext() (ok bool) {
+    this.position++;
+    if this.position >= len(this.data) {
+        this.position--;
         return false;
     }
-    s.x--;
+    this.position--;
     return true;
 };
 
-func (s *IterableSlice) GetS() [][]float64 {
-    return s.s;
+func (this *IterableSlice) GetS() [][]float64 {
+    return this.data;
 };
 
-func (s *IterableSlice) Reset() {
-  s.x = -1;
+func (this *IterableSlice) Reset() {
+  this.position = -1;
 }
 
-func NewIterator(s [][]float64) *IterableSlice {
-    return &IterableSlice{-1, s};
+func NewIterator(data [][]float64) *IterableSlice {
+    return &IterableSlice{-1, data, nil};
 };
