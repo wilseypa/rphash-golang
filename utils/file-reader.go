e7f01c7fbca42ebeeb3d9e09bb06fde75a330f58
Jacob <frankljbe@gmail.com>
2016-05-04 13:39:17 -0400

    [test]Tests now use buffered readers

--- utils/file-reader.go
+++ utils/file-reader.go
@@ -8,6 +8,55 @@ import (
     "strings"
 	"strconv"
 )
+type DataFileReader struct {
+  reader          	*bufio.Reader
+  file				*os.File
+  buffer			*bytes.Buffer
+  hasNext			bool
+  part				[]byte
+  prefix			bool
+  err 				error
+}
+
+
+func NewDataFileReader(path string) *DataFileReader {
+	var (
+		file   *os.File
+		err    error
+	)
+	if file, err = os.Open(path); err != nil {
+		panic(err)
+	}
+	
+	reader := bufio.NewReader(file)
+	buffer := bytes.NewBuffer(make([]byte, 0))
+	hasNext := true
+	return &DataFileReader{
+		reader:       	   reader,
+		buffer:            buffer,
+		hasNext:           hasNext,
+		file:			   file,
+  }
+}
+
+func (this *DataFileReader) HasNext() bool {
+	return this.hasNext
+}
+
+func (this *DataFileReader) Next() []float64 {
+		for {
+			if this.part, this.prefix, this.err = this.reader.ReadLine(); this.err != nil {
+				this.hasNext = false
+				return nil
+			}
+			this.buffer.Write(this.part)
+			if !this.prefix {
+				line := strings.Fields(this.buffer.String())
+				this.buffer.Reset()
+				return StringLineToFloatLine(line)
+			}
+		}
+}
 
 func ReadXLines(path string, x int) (lines [][]string, err error) {
 	// Read a whole file into the memory and store it as array of lines
@@ -53,6 +102,18 @@ func StringArrayToFloatArray(lines [][]string) (result [][]float64) {
         }
     }
     return result;
+}
+
+func StringLineToFloatLine(line []string) (result []float64) {
+	    result = make([]float64, len(line), len(line))
+        for j, toFloat := range line {
+			float, err := strconv.ParseFloat(toFloat, 64)
+			if err != nil {
+				panic(err)
+			}
+           	result[j] = float
+        }
+		return result;
 }
 
 func ReadLines(path string) (lines [][]string, err error) {
